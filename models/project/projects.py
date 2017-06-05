73dbffddc9ba4dea2c8ca8e770e4e79ec2920972
phil_steel <quinniehernaez@gmail.com>
2017-06-05 10:06:20 +0800

    modified search

--- models/project/projects.py
+++ models/project/projects.py
@@ -6,10 +6,10 @@ from odoo import models, fields, api
 class Projects(models.Model):
 	_name = 'philsteel.projects'
 
-	name = fields.Char(string="Project Name", required=True)
+	name = fields.Char(string="Project Name", required='True')
 	location = fields.Text(string="Location", required='True')
 	image = fields.Binary()
-	customer_name = fields.Char(string="Customer Name", required=True)
-	ic_no = fields.Char(string="IC No", required=True)
-	sc_no = fields.Char(string="SC No", required=True)
+	customer_name = fields.Char(string="Customer Name", required='True')
+	ic_no = fields.Char(string="IC No", required='True')
+	sc_no = fields.Char(string="SC No", required='True')
 	types_of_project = fields.Selection([('residential', 'Residential'), ('commercial', 'Commercial'), ('industrial', 'Industrial'), ('government', 'Government'), ('institutional', 'Institutional'), ('mass_housing', 'Mass Housing')], string='Type of Project')
