76f873a159b9f5e7f8b84ab4b6721c8ba451ca7a
phil_steel <quinniehernaez@gmail.com>
2017-06-13 12:21:25 +0800

    modified model security

--- models/project_activities/project_activities.py
+++ models/project_activities/project_activities.py
@@ -8,10 +8,3 @@ class ProjectActivities(models.Model):
 	name = fields.Char(string="Activity", required=True)
 	status = fields.Selection([('new', 'New'), ('ongoing', 'Ongoing'),('done', 'Done'), ('onhold', 'On-hold'), ('cancelled', 'cancelled')], string='Activity Status')
 
-
-class Departments(models.Model):
-	_name = 'philsteel.departmentx'
-
-	department_name = fields.Char(string="Department", required=True)
-	description = fields.Text(string="Description")
-
